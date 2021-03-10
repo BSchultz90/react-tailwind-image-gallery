e2700d1c341bf91169b16c9657465a481f1afcf2
Brice Schultz <Briceschutz91@gmail.com>
2021-03-09 20:28:57 -0700

    Fixed API key issue / adding .env to .gitignore

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+/* eslint-disable no-unused-vars */
+// Imports
 import React, { useState, useEffect } from "react";
 
+// App Level Component
 function App() {
+  // State Set Below * State Always needs a Default Value
   const [images, setImages] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [term, setTerm] = useState("");
 
+  // HTTP Request using useEffect & Fetch. *Fetch Returns an Object that needs to be Stringified with .json()
   useEffect(() => {
-    fetch(``);
-  });
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}q=${term}&image_type=photo`
+    )
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
