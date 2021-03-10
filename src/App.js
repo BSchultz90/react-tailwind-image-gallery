5b54e4e74ba48fa7b2e6dd756b5662c7cc2e372e
Brice Schultz <Briceschutz91@gmail.com>
2021-03-09 21:32:49 -0700

    EOD Commit / 80% done / TS@29:06

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 // Imports
 import React, { useState, useEffect } from "react";
+import ImageCard from "./components/ImageCard";
 
 // App Level Component
 function App() {
@@ -12,47 +13,22 @@ function App() {
   // HTTP Request using useEffect & Fetch. *Fetch Returns an Object that needs to be Stringified with .json()
   useEffect(() => {
     fetch(
-      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}q=${term}&image_type=photo`
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo`
     )
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
       .catch((err) => console.log(err));
   }, []);
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
-      ;
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2">
-          Photo by John Doe
-        </div>
-        <ul>
-          <li>
-            <strong>Views: 4000 </strong>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <strong>Downloads: 300 </strong>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <strong>Likes: </strong>
-          </li>
-        </ul>
-      </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag1
-        </span>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        {images.map((image) => (
+          <ImageCard key={image.id} image={image} />
+        ))}
       </div>
     </div>
   );
